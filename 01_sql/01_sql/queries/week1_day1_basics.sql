f3dd242515ee8fc3dcd7b86442a1cbefd5e185ad
BWill-iams <bbwilliams760@gmail.com>
2026-01-06 20:04:12 -0600

    Add files via upload
    Week 1 Day 1: SQL basics and filtering

---
+++ 01_sql/01_sql/queries/week1_day1_basics.sql
@@ -0,0 +1,39 @@
+Queries to Write
+-- Access the view of all customers
+Select *
+FROM customers;
+
+-- Pull data from customer_id, gender and tenure columns from customers table
+Select customer_id, gender, tenure,
+FROM customers;
+
+-- Filtering customers whose tenure is 12 months or longer
+SELECT customer_id, tenure
+FROM customers
+WHERE tenure > 12;
+
+--Filter customers that are on month-to-month contracts
+SELECT customer_id, contract_type
+FROM customers
+WHERE contract_type = 'Month-to-month';
+
+-- Sort customers by tenure (highest to lowest)
+SELECT customers_id, tenure
+FROM customers
+ORDER BY tenure DESC;
+
+-- View list of Top 10 Longest Tenured Customers
+SELECT customer_id, tenure
+FROM customers
+ORDER BY tenure DESC
+LIMIT 10;
+
+-- Select UNIQUE contract types
+SELECT DISTINCT contract_type
+FROM customers;
+
+-- List of customers who are senior citizens
+SELECT customer_id, senior_citizen
+FROM customers
+WHERE senior_citizen = 1;
+-
