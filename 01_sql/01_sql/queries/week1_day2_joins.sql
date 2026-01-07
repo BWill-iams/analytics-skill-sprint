c8f0566d8ba2e808b3966b90a6143a750ec7bf72
BWill-iams <bbwilliams760@gmail.com>
2026-01-06 20:31:46 -0600

    Update week1_day2_joins.sql
    Week 1 Day 2: SQL joins and cross-table analysis

--- 01_sql/01_sql/queries/week1_day2_joins.sql
+++ 01_sql/01_sql/queries/week1_day2_joins.sql
@@ -1,1 +1,55 @@
 
+-- Combine customer demographics with billing information
+SELECT
+  c.customer_id,
+  c.gender,
+  c.tenure,
+  b.monthly_charges,
+  b.payment_method
+FROM customers c
+INNER JOIN billing b
+  ON c.customer_id = b.customer_id;
+
+-- Identifying customers who have churned
+SELECT
+  c.customer_id,
+  c.tenure,
+  b.churn
+FROM customers c
+INNER JOIN billing b
+  ON c.customer_id = b.customer_id
+WHERE b.churn = 'Yes';
+
+-- List services used by customers who have churned
+SELECT 
+  c.customer_id.
+  s.internet_service,
+  s.streaming,
+  s.tech_support,
+  b.churn
+FROM customers c
+INNER JOIN services s
+  ON c.customer_id = s.customer_id
+INNER JOIN billing b
+  ON c.customer_id = b.customer_id
+WHERE b.churn = 'Yes';
+
+-- Identify customers without services
+-- may indicate inactive accounts or gaps in data
+SELECT
+  c.sutomer_id,
+  s.internet_service
+FROM customers c
+LEFT JOIN services s
+  ON c.customer_id = s.customer_id
+WHERE s.customer_id IS NULL;
+
+-- Compare charges by contract type
+SELECT
+  c.contract_type,
+  AVG(b.monthly_charges) AS avg_monthly_charges
+FROM customers c
+INNER JOIN billing b
+  ON c.customer_id = b.customer_id
+GROUP BY c.contract_type;
+
